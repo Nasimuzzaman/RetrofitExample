2339ccf81c2af209c732d41be65b8abc8d63b389
Unknown <bsse0602@iit.du.ac.bd>
2017-10-07 21:10:30 +0600

    initial commit retrofit example

---
+++ includes/Constants.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nasim
+ * Date: 07-Oct-17
+ * Time: 5:22 PM
+ */
+
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'retrofit');
+
+define('USER_CREATED', 101);
+define('USER_EXIST', 102);
+define('USER_CREATION_FAILED', 103);
